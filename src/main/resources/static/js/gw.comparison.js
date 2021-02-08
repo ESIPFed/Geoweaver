8bf62702d93530dd32c7ab1c803c21c50154c7ff
ZihengSun <zsun@gmu.edu>
2021-02-08 01:49:36 -0500

    add compare button

--- src/main/resources/static/js/gw.comparison.js
+++ src/main/resources/static/js/gw.comparison.js
@@ -5,18 +5,24 @@
  */
 
 GW.comparison = {
-		
+	
+	list_hist_ids: [],
+	
 	list_history: [],
+	
+	compare_frame: null,
 		
 	show: function(){
 
+		this.list_hist_ids = [];
+		
 		this.list_history = []; //clear the current comparison
 		
 		var count = 0;
 		
 		$(".hist-checkbox:checked").each(function() {
 			
-			var histid = $(this).attr('id');
+			var histid = $(this).attr('id').substring(9);
 			
 			count+=1;
 			
@@ -52,49 +58,23 @@ GW.comparison = {
 				
 				var code = msg.output;
 				
-				GW.comparison.list_history.append(code);
-				
-				if (GW.comparison.list_history.length==2) {
+				if(code!=null && typeof code != 'undefined'){
 					
-
-					if(code!=null && typeof code != 'undefined'){
+					if(typeof code != 'object'){
+					
+						code = $.parseJSON(code);
+					
+					}
+				
+					GW.comparison.list_history.push(code);
+					
+					GW.comparison.list_hist_ids.push(histid);
+				
+					if (GW.comparison.list_history.length==2) {
 						
-						if(typeof code != 'object'){
-						
-							code = $.parseJSON(code);
-						
-						}
-						
-						var notebook = nb.parse(code.content);
-						
-						var rendered = notebook.render();
-						
-						var content = '<div class="modal-body">'+$(rendered).html()+'</div>';
-						
-						content += '<div class="modal-footer">' +
-	    				"	<button type=\"button\" id=\"host-history-download-btn\" class=\"btn btn-outline-primary\">Download</button> "+
-	    				"	<button type=\"button\" id=\"host-history-cancel-btn\" class=\"btn btn-outline-secondary\">Cancel</button>"+
-	    				'</div>';
-						
-//						console.log(content);
-						
-						GW.host.his_frame = GW.process.createJSFrameDialog(800, 600, content, "History Jupyter Notebook " + history_id);
-						
-						$("#host-history-download-btn").click(function(){
-							
-							GW.host.downloadJupyter(history_id);
-							
-						})
-						
-						$("#host-history-cancel-btn").click(function(){
-	        				
-	        				GW.host.his_frame.closeFrame();
-	        				
-	        			});
+						GW.comparison.showDialog();
 						
 					}
-					
-					
 					
 				}
 				
@@ -107,6 +87,61 @@ GW.comparison = {
 		    
 		});
 
+	},
+	
+	showDialog: function(){
+		
+		var notebook1 = nb.parse(GW.comparison.list_history[0].content);
+		
+		var notebook2 = nb.parse(GW.comparison.list_history[1].content);
+		
+		var rendered1 = notebook1.render();
+		
+		var rendered2 = notebook2.render();
+		
+		var content = '<div class="modal-body">'+
+				
+				'	<div class="row">'+
+				
+				'		<div class="col-md-6">'+
+				
+				$(rendered1).html()+
+				
+				'		</div>'+
+				
+				'		<div class="col-md-6">'+
+				
+				$(rendered2).html()+
+				
+				'		</div>'+
+				
+				'	</div>'+
+				
+				'</div>';
+		
+		content += '<div class="modal-footer">' +
+		"	<button type=\"button\" id=\"compare-download-btn\" class=\"btn btn-outline-primary\">Download</button> "+
+		"	<button type=\"button\" id=\"compare-cancel-btn\" class=\"btn btn-outline-secondary\">Cancel</button>"+
+		'</div>';
+		
+//		console.log(content);
+		
+		GW.comparison.compare_frame = GW.process.createJSFrameDialog(800, 600, content, "History Comparison " + GW.comparison.list_hist_ids[0] + 
+				" vs " + GW.comparison.list_hist_ids[1]);
+		
+		$("#compare-download-btn").click(function(){
+			
+			GW.host.downloadJupyter(GW.comparison.list_hist_ids[0]);
+			GW.host.downloadJupyter(GW.comparison.list_hist_ids[1]);
+			
+		})
+		
+		$("#compare-cancel-btn").click(function(){
+			
+			GW.comparison.compare_frame.closeFrame();
+			
+		});
+		
 	}
 		
 		
