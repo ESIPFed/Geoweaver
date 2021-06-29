8689bd67a18dfd6fc324cce517ef01d26bcb8544
Ahmed Aln(u)aim <ahmed.y.alnaim@gmail.com>
2021-06-30 00:30:58 +0300

    fix #184 (code section & logging side by side in process tab)

--- src/main/resources/static/js/gw.process.js
+++ src/main/resources/static/js/gw.process.js
@@ -1093,7 +1093,9 @@ GW.process = {
 		    '   </div>';
 			
 			content += "<div class=\"row\" style=\"font-size: 12px;\">"+
-				"<div class=\"col col-md-12\" ><h4 class=\"border-bottom\">Code Section <button type=\"button\" class=\"btn btn-secondary btn-sm\" id=\"showCurrent\">Latest Code</button></h4> </div>";
+				"<div class=\"row\"><div class=\"col col-md-6\" ><h4 class=\"border-bottom\">Code Section <button type=\"button\" class=\"btn btn-secondary btn-sm\" id=\"showCurrent\">Latest Code</button></h4> <div class=\"col col-md-6\" id=\"code-embed\" style=\"width:100%; \" ></div></div> <div id=\"main-console-content\" style=\"height:100%; overflow-y: scroll; margin:0; padding: 10px;\"> <h4 style=\"color:black\">logging information and errors</h4> <div id=\"log-window\" style=\"padding-left: 8px;padding-top: 19px; overflow-wrap: break-word;\"> </div> </div></div>";
+
+			// content += "<div class=\"col col-md-6\" id=\"code-embed\" style=\"/* width:100%; */; float: none;\" ></div>";
 			
 			var code = null;
 			
@@ -1116,7 +1118,6 @@ GW.process = {
 			
 			process_name = msg.name;
 			
-			content += "<div class=\"col col-md-12\" id=\"code-embed\" style=\"width:100%;\" ></div>";
 			
 			content += '</div>'+
 			'   <div class="row" style="padding:0px;margin:0px;" >'+
