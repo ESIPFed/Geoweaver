745dbcb8f620def7d1ecf0a4be0c862ba9d5d901
ZihengSun <zsun@gmu.edu>
2021-07-13 00:34:32 -0400

    add login dialog

--- src/main/resources/static/js/gw.user.js
+++ src/main/resources/static/js/gw.user.js
@@ -17,9 +17,35 @@ GW.user = {
      */
     login_dialog: function(){
 
-        var content = "<div class=\"modal-body\" style=\"font-size: 12px;\">";
-				
-        content += "</div>";
+        var content = "<div class=\"modal-body\" style=\"font-size: 12px;\">"+
+        
+        "<div class=\"row\">"+
+        
+        "   <div class=\"col-md-12\">"+
+        '       <input type="text" id="login"  name="login" placeholder="login">'+
+        "   </div>"+
+        "   <div class=\"col-md-12\">"+
+        '       <input type="text" id="password" name="password" placeholder="password">'+
+        "   </div>"+
+        "</div>"+
+
+        "<div class=\"row\">"+
+        "   <div class=\"col-md-12\" >"+
+        "       <button click=\"GW.user.login()\">Sign In</button>"+
+        "       <button click=\"GW.user.signupdialog()\">Sign Up</button>"+
+        '   </div>'+
+        "</div>"+
+
+        "<div class=\"row\">"+
+        '   <div class=\"col-md-12\">'+
+        
+        "       <button click=\"GW.user.forgetpassdialog()\">Forgot Password?</button>"+
+
+        "   </div>"+
+        "</div>"+
+        
+        
+        "</div>";
         
         var frame = GW.process.createJSFrameDialog(300, 300, content, "Login")
 
@@ -31,6 +57,12 @@ GW.user = {
     login: function(){
 
 
+
+    },
+
+    signupdialog: function(){
+
+        
 
     },
 
