8376ea3d3b083f3f50eca786cfa5acae5c3510b8
ZihengSun <zsun@gmu.edu>
2021-08-28 09:03:52 -0400

    add the front dialogs for user management

--- src/main/resources/static/js/gw.user.js
+++ src/main/resources/static/js/gw.user.js
@@ -30,24 +30,24 @@ GW.user = {
         "<div class=\"row\">"+
         
         "   <div class=\"col-md-12\">"+
-        '       <input type="text" id="username"  name="username" placeholder="username">'+
+        '       <input type="text" id="username" class=\"input-lg\" name="username" placeholder="username">'+
         "   </div>"+
         "   <div class=\"col-md-12\">"+
-        '       <input type="text" id="password" name="password" placeholder="password">'+
+        '       <input type="text" id="password" class=\"input-lg\" name="password" placeholder="password">'+
         "   </div>"+
         "</div>"+
 
         "<div class=\"row\">"+
         "   <div class=\"col-md-12\" >"+
-        "       <button click=\"GW.user.login()\">Sign In</button>"+
-        "       <button click=\"GW.user.signupdialog()\">Sign Up</button>"+
+        "       <button onclick=\"GW.user.login()\" class=\"btn btn-lg btn-primary\" >Sign In</button>"+
+        "       <button onclick=\"GW.user.signupdialog()\"  class=\"btn btn-lg btn-primary\">Sign Up</button>"+
         '   </div>'+
         "</div>"+
 
         "<div class=\"row\">"+
         '   <div class=\"col-md-12\">'+
         
-        "       <button click=\"GW.user.forgetpassdialog()\">Forgot Password?</button>"+
+        "       <button onclick=\"GW.user.forgetpassdialog()\"  class=\"btn btn-lg btn-primary\">Forgot Password?</button>"+
 
         "   </div>"+
         "</div>"+
@@ -55,7 +55,31 @@ GW.user = {
         
         "</div>";
         
-        var frame = GW.process.createJSFrameDialog(300, 300, content, "Login")
+        this.login_frame = GW.process.createJSFrameDialog(300, 400, content, "Login")
+
+    },
+
+    precheck: function(){
+
+        var isvalid = true;
+
+        if(!$("#username").val()){
+
+            isvalid = false;
+
+        }else if(!$("#password").val()){
+
+            isvalid = false;
+
+        }
+
+        if(!isvalid){
+
+            alert("Either usename or password is missing. ");
+
+        }
+
+        return isvalid;
 
     },
 
@@ -64,7 +88,31 @@ GW.user = {
      */
     login: function(){
 
+        if(this.precheck()){
 
+            //password need to be encrypted first
+
+            $.ajax({
+
+                url: "signin",
+				
+                method: "POST",
+            
+                data: { usename: $("#username").val(), password: $("#password").val()}
+
+            }).done(function(msg){
+
+                msg = $.parseJSON(msg);
+
+
+
+            }).fail(function(jxr, status){
+
+                console.error("Sign In Failed");
+
+            });
+
+        }
 
     },
 
@@ -73,48 +121,7 @@ GW.user = {
      */
     signup: function(){
 
-        GW.user.closeOtherFrames();
 
-        var content = "<div class=\"modal-body\" style=\"font-size: 12px;\">"+
-        
-        "<div class=\"row\">"+
-        
-        "   <div class=\"col-md-12\">"+
-        '       User Name: <input type="text" id="username"  name="username" placeholder="username">'+
-        "   </div>"+
-        "   <div class=\"col-md-12\">"+
-        '       Enter Password: <input type="text" id="password" name="password" placeholder="new password">'+
-        "   </div>"+
-        "   <div class=\"col-md-12\">"+
-        '       Re-enter Password: <input type="text" id="rpassword" name="rpassword" placeholder="re-enter password">'+
-        "   </div>"+
-        "   <div class=\"col-md-12\">"+
-        '       Email: <input type="text" id="email" name="email" placeholder="email">'+
-        "   </div>"+
-        "   <div class=\"col-md-12\">"+
-        '       Agree terms: <input type="text" id="email" name="email" placeholder="email">'+
-        "   </div>"+
-        "</div>"+
-
-        "<div class=\"row\">"+
-        "   <div class=\"col-md-12\" >"+
-        "       <button click=\"GW.user.register()\">Register</button>"+
-        "       <button click=\"GW.user.closeOtherFrames()\">Cancel</button>"+
-        '   </div>'+
-        "</div>"+
-
-        "<div class=\"row\">"+
-        '   <div class=\"col-md-12\">'+
-        
-        "       <button click=\"GW.user.forgetpassdialog()\">Forgot Password?</button>"+
-
-        "   </div>"+
-        "</div>"+
-        
-        
-        "</div>";
-        
-        var frame = GW.process.createJSFrameDialog(300, 300, content, "Login")
 
     },
 
@@ -130,6 +137,8 @@ GW.user = {
 
     signupdialog: function(){
 
+        console.log("Enter sign up dialog..");
+
         GW.user.closeOtherFrames();
 
         var content = "<div class=\"modal-body\" style=\"font-size: 12px;\">"+
@@ -137,34 +146,80 @@ GW.user = {
         "<div class=\"row\">"+
         
         "   <div class=\"col-md-12\">"+
-        '       <input type="text" id="login"  name="login" placeholder="login">'+
+        '       <input type="text" id="username" class=\"input-lg\" name="username" placeholder="username"/>'+
         "   </div>"+
         "   <div class=\"col-md-12\">"+
-        '       <input type="text" id="password" name="password" placeholder="password">'+
+        '       <input type="text" id="password" class=\"input-lg\" name="password" placeholder="new password"/>'+
+        "   </div>"+
+        "   <div class=\"col-md-12\">"+
+        '       <input type="text" id="rpassword" class=\"input-lg\" name="rpassword" placeholder="re-enter password"/>'+
+        "   </div>"+
+        "   <div class=\"col-md-12\">"+
+        '       <input type="text" id="email" class=\"input-lg\" name="email" placeholder="email"/>'+
+        "   </div>"+
+        "   <div class=\"col-md-12\">"+
+        '       Agree terms: <input type="text" id="email" class=\"input-lg\" name="email" placeholder="email"/>'+
         "   </div>"+
         "</div>"+
 
         "<div class=\"row\">"+
         "   <div class=\"col-md-12\" >"+
-        "       <button click=\"GW.user.signup()\">Register</button>"+
-        "       <button click=\"GW.user.signupdialog()\">Cancel</button>"+
+        "       <button onclick=\"GW.user.register()\"  class=\"btn btn-lg btn-primary\">Register</button>"+
+        "       <button onclick=\"GW.user.closeOtherFrames()\" class=\"btn btn-lg btn-primary\">Cancel</button>"+
         '   </div>'+
         "</div>"+
 
         "<div class=\"row\">"+
-        
         '   <div class=\"col-md-12\">'+
         
-        "       Already has an account? Click to <button click=\"GW.user.login_dialog()\">Sign In</button>"+
+        "       <button onclick=\"GW.user.forgetpassdialog()\" class=\"btn btn-lg btn-primary\">Forgot Password?</button>"+
 
         "   </div>"+
-
         "</div>"+
         
         
         "</div>";
         
-        var frame = GW.process.createJSFrameDialog(300, 300, content, "Sign Up")
+        this.signup_frame = GW.process.createJSFrameDialog(400, 400, content, "Registration")
+
+    },
+
+    register: function(){
+
+        if(this.precheck()){
+
+            $.ajax({
+
+                url: "forgetpassword",
+				
+                method: "POST",
+            
+                data: { recover_email: $("#recover_email").val()}
+
+            }).done(function(msg){
+    
+    
+    
+            }).fail(function(jxr, status){
+    
+    
+            });
+
+        }
+
+    },
+
+    resetPassword: function(){
+
+        if(this.precheck()){
+
+            $.ajax({
+
+
+
+            });
+
+        }
 
     },
 
@@ -174,35 +229,23 @@ GW.user = {
 
         var content = "<div class=\"modal-body\" style=\"font-size: 12px;\">"+
         
-        "<div class=\"row\">"+
+        "   <div class=\"row\">"+
         
-        "   <div class=\"col-md-12\">"+
-        '       <input type="text" id="login"  name="login" placeholder="login">'+
+        "      <div class=\"col-md-12\">"+
+        '          <input type="text" id="recover_email"  class=\"input-lg\" name="recover_email" placeholder="email address">'+
+        "      </div>"+
         "   </div>"+
-        "   <div class=\"col-md-12\">"+
-        '       <input type="text" id="password" name="password" placeholder="password">'+
+
+        "   <div class=\"row\">"+
+        "      <div class=\"col-md-12\" >"+
+        "          <button onclick=\"GW.user.resetPassword()\"  class=\"btn btn-lg btn-primary\">Send</button>"+
+        "          <button onclick=\"GW.user.closeOtherFrames()\"  class=\"btn btn-lg btn-primary\">Cancel</button>"+
+        '      </div>'+
         "   </div>"+
-        "</div>"+
-
-        "<div class=\"row\">"+
-        "   <div class=\"col-md-12\" >"+
-        "       <button click=\"GW.user.login()\">Sign In</button>"+
-        "       <button click=\"GW.user.signupdialog()\">Sign Up</button>"+
-        '   </div>'+
-        "</div>"+
-
-        "<div class=\"row\">"+
-        '   <div class=\"col-md-12\">'+
-        
-        "       <button click=\"GW.user.forgetpassdialog()\">Forgot Password?</button>"+
-
-        "   </div>"+
-        "</div>"+
-        
         
         "</div>";
         
-        var frame = GW.process.createJSFrameDialog(300, 300, content, "Login")
+        this.forget_password_frame = GW.process.createJSFrameDialog(400, 400, content, "Login")
 
     },
 
