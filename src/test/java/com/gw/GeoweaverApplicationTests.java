61c83dc876fd0393da8ebc6bbda1c23981b2e6b5
ZihengSun <zsun@gmu.edu>
2021-09-21 22:15:12 -0400

    add unit test

--- src/test/java/com/gw/GeoweaverApplicationTests.java
+++ src/test/java/com/gw/GeoweaverApplicationTests.java
@@ -1,16 +1,123 @@
 package com.gw;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+import com.gw.jpa.GWUser;
+import com.gw.tools.UserTool;
+import com.gw.web.GeoweaverController;
+
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GeoweaverApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testrestTemplate;
+
+
+	Logger logger  = Logger.getLogger(this.getClass());
+
+	@Autowired
+	UserTool ut;
 
 	@Test
 	void contextLoads() {
 		
 		
+	}
+
+	@Test
+	@DisplayName("Testing adding/editing/removing user...")
+	void testUser(){
 		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+    	HttpEntity request = new HttpEntity<>("type=host", headers);
+		String result = this.testrestTemplate.postForObject("http://localhost:" + this.port + "/Geoweaver/web/list", 
+			request, 
+			String.class);
+		logger.debug("the result is: " + result);
+		// assertThat(controller).isNotNull();
+		assertThat(result).contains("[");
+
+	}
+
+	@Test
+   	@DisplayName("Subscription message service test ")
+   	void testSubscriptionMessage() {
+		
+      	GWUser u = ut.getUserById("111111");
+
+      	assertEquals(u.getUsername(), "publicuser");
+   	}
+
+	@Test
+	@DisplayName("Testing if the front page is accessible..")
+	void testFrontPage(){
+		String result = this.testrestTemplate.getForObject("http://localhost:" + this.port + "/Geoweaver/web/geoweaver", String.class);
+		// logger.debug("the result is: " + result);
+		// assertThat(controller).isNotNull();
+		assertThat(result).contains("Geoweaver");
+		
+	}
+
+	@Test
+	@DisplayName("Testing Dashboard...")
+	void testDashboard(){
+		// ResponseEntity<String> result = testrestTemplate.getForEntity("http://localhost:" + this.port + "/Geoweaver/web/dashboard", String.class);
+		ResponseEntity result = this.testrestTemplate.postForEntity("http://localhost:" + this.port + "/Geoweaver/web/dashboard",
+			"",
+			String.class);
+		// logger.debug("the dashboard result is: " + result);
+		// assertThat(controller).isNotNull();
+		assertEquals(200, result.getStatusCode().value());
+		assertThat(result.getBody().toString()).contains("process_num");
+	}
+
+	@Test
+	@DisplayName("Testing list of host, process, and workflow...")
+	void testList(){
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+    	HttpEntity request = new HttpEntity<>("type=host", headers);
+		String result = this.testrestTemplate.postForObject("http://localhost:" + this.port + "/Geoweaver/web/list", 
+			request, 
+			String.class);
+		logger.debug("the result is: " + result);
+		// assertThat(controller).isNotNull();
+		assertThat(result).contains("[");
+
+		request = new HttpEntity<>("type=process", headers);
+		result = this.testrestTemplate.postForObject("http://localhost:" + this.port + "/Geoweaver/web/list", 
+			request, 
+			String.class);
+		// logger.debug("the result is: " + result);
+		// assertThat(controller).isNotNull();
+		assertThat(result).contains("[");
+
+		request = new HttpEntity<>("type=workflow", headers);
+		result = this.testrestTemplate.postForObject("http://localhost:" + this.port + "/Geoweaver/web/list", 
+			request, 
+			String.class);
+		// logger.debug("the result is: " + result);
+		// assertThat(controller).isNotNull();
+		assertThat(result).contains("[");
 	}
 
 }
