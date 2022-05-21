d43502192b2e60591bfc30d0d0147476609e8fe6
Pavan Kumar D <kumarpavan1309@gmail.com>
2022-05-21 15:11:08 -0400

    added detail command, added help command, added tables for displaying results on cli

---
+++ src/main/java/com/gw/commands/DetailCommand.java
@@ -0,0 +1,123 @@
+package com.gw.commands;
+
+import java.util.Optional;
+
+import com.gw.database.ProcessRepository;
+import com.gw.database.WorkflowRepository;
+import com.gw.jpa.GWProcess;
+import com.gw.jpa.Host;
+import com.gw.jpa.Workflow;
+import com.gw.tools.HostTool;
+import com.gw.utils.BeanTool;
+import com.gw.utils.CommandLineUtil;
+
+import org.springframework.stereotype.Component;
+
+import picocli.CommandLine.ArgGroup;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+
+class requiredDetailCommandOptions {
+
+    @Option(names = { "--host-id" }, description = "Host Id to get details for")
+    String hostId;
+
+    @Option(names = { "--process-id" }, description = "Process Id to get details for")
+    String processId;
+
+    @Option(names = { "--workflow-id" }, description = "Workflow Id to get details for")
+    String workflowId;
+
+}
+
+@Component
+@Command(name = "detail", description = "Show the detail of a resource")
+public class DetailCommand implements Runnable {
+    
+    @ArgGroup(exclusive = false, multiplicity = "1")
+    requiredDetailCommandOptions requiredCommandOptions;
+
+    public void run() {
+
+        CommandLineUtil.CommandLineTable table = new CommandLineUtil.CommandLineTable();
+
+        if(requiredCommandOptions.hostId != null) {
+            System.out.println("HostId: " + requiredCommandOptions.hostId);
+            HostTool ht = BeanTool.getBean(HostTool.class);
+
+            Host host = ht.getHostById(requiredCommandOptions.hostId);
+
+            if(host != null) {
+
+                table.setHeaders(new String[] { "Host Id", "Hostname", "IP", "Port", "Username", "Owner", "type", "url", "Confidential?" });
+                table.addRow(host.getId(), host.getName(), host.getIp(), host.getPort(), host.getUsername(), host.getOwner(), host.getType(), host.getUrl(), host.getConfidential());
+
+            }else {
+
+                table.setHeaders(new String[] { "Input", "Error" });
+                table.addRow("HostId", "No host found with id: " + requiredCommandOptions.hostId);
+
+            }
+
+
+        } else if(requiredCommandOptions.processId != null) {
+
+            ProcessRepository pr = BeanTool.getBean(ProcessRepository.class);
+
+            Optional<GWProcess> process = pr.findById(requiredCommandOptions.processId);
+
+            if(process.isPresent()) {
+                GWProcess p = process.get();
+                // Set Headers for table
+                table.setHeaders(new String[] { "Process Id", "Process Name", "Process Description", "Process Language", "Process Owner", "Process is Confidential?" });
+                
+                // Add row to table
+                table.addRow(p.getId(), p.getName(), p.getDescription(), p.getLang(), p.getOwner(), p.getConfidential());
+
+            } else {
+
+                // No process found
+                // Set error headers
+                table.setHeaders(new String[] { "Input", "Error" });
+
+                // Add error row
+                table.addRow("Process Id", "No process found with id: " + requiredCommandOptions.processId);
+
+            }
+
+        } else if(requiredCommandOptions.workflowId != null) {
+
+            WorkflowRepository workflowrepository = BeanTool.getBean(WorkflowRepository.class);
+
+            Optional<Workflow> workflow = workflowrepository.findById(requiredCommandOptions.workflowId);
+
+            if(workflow.isPresent()) {
+
+                Workflow w = workflow.get();
+
+                // Set Headers for table
+                table.setHeaders(new String[] { "Workflow Id", "Name", "Description", "Owner", "Confidential?" });
+
+                // Add row to table
+                table.addRow(w.getId(), w.getName(), w.getDescription(), w.getOwner(), w.getConfidential());
+
+            } else {
+
+                // No workflow found
+                // Set error headers
+                table.setHeaders(new String[] { "Input", "Error" });
+
+                // Add error row
+                table.addRow("Workflow Id", "No workflow found with id: " + requiredCommandOptions.workflowId);
+
+            }
+
+            System.out.println("WorkflowId: " + requiredCommandOptions.workflowId);
+        }
+
+        // Print table
+        table.print();
+    }
+
+}
--- src/main/java/com/gw/commands/ListCommand.java
+++ src/main/java/com/gw/commands/ListCommand.java
@@ -1,24 +1,25 @@
 package com.gw.commands;
 
-import java.io.Console;
 import java.util.Collection;
 import java.util.List;
 
 import com.gw.database.ProcessRepository;
 import com.gw.database.WorkflowRepository;
+import com.gw.jpa.GWProcess;
 import com.gw.jpa.Host;
 import com.gw.jpa.Workflow;
 import com.gw.tools.HostTool;
 import com.gw.utils.BeanTool;
+import com.gw.utils.CommandLineUtil;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-class requiredCommandOptions {
+class requiredListCommandOptions {
+
     @Option(names = { "--host" }, description = "list hosts")
     Boolean host;
 
@@ -27,42 +28,60 @@ class requiredCommandOptions {
 
     @Option(names = { "--workflow" }, description = "list workflows")
     Boolean workflow;
+
 }
 
 @Component
 @Command(name = "list", description = "list the resources in Geoweaver")
 public class ListCommand implements Runnable {
 
-    @Autowired
-    ProcessRepository processrepository;
-
-    @Autowired
-    WorkflowRepository workflowrepository;
-
     @ArgGroup(exclusive = false, multiplicity = "1")
-    requiredCommandOptions requiredCommandOptions;
+    requiredListCommandOptions requiredCommandOptions;
     
 
     public void run() {
-        Console console = System.console();
+        CommandLineUtil.CommandLineTable table = new CommandLineUtil.CommandLineTable();
         if(requiredCommandOptions.host != null && requiredCommandOptions.host) {
             HostTool ht = BeanTool.getBean(HostTool.class);
             List<Host> allhosts = ht.getAllHosts();
-            console.printf("Listing hosts (%d)%n", allhosts.size());
-            console.printf("format: id - name - ip - port - username%n");
-            for(Host host : allhosts) {
-                console.printf("%s - %s - %s - %s - %s%n", host.getId(), host.getName(), host.getIp(), host.getPort(), host.getUsername());
+
+            table.setHeaders(new String[] { "Host Id", "Hostname", "IP", "Port", "Username" });
+
+            for(Host h : allhosts) {
+                table.addRow(h.getId(), h.getName(), h.getIp(), h.getPort(), h.getUsername());
             }
+
         }else if(requiredCommandOptions.process != null && requiredCommandOptions.process) {
-            console.printf("Listing processes (%d)%n", processrepository.count());
-        }else if(requiredCommandOptions.workflow != null && requiredCommandOptions.workflow) {
-            console.printf("Listing workflows (%d)%n", workflowrepository.count());
-            console.printf("format: id - name%n");
-            Collection<Workflow> workflows = (Collection<Workflow>) workflowrepository.findAll();
-            for(Workflow workflow:workflows) {
-                console.printf("%s - %s%n", workflow.getId(), workflow.getName());
+            ProcessRepository pr = BeanTool.getBean(ProcessRepository.class);
+
+            Collection<GWProcess> allPublicProcesses = pr.findAllPublic();
+
+            // Set Headers
+            table.setHeaders(new String[] { "Process Id", "Name", "Language", "Description" });
+
+            // Add Rows
+            for(GWProcess p : allPublicProcesses) {
+                table.addRow(p.getId(), p.getName(), p.getLang(), p.getDescription());
             }
+
+        }else if(requiredCommandOptions.workflow != null && requiredCommandOptions.workflow) {
+            
+            WorkflowRepository workflowrepository = BeanTool.getBean(WorkflowRepository.class);
+            
+            Collection<Workflow> workflows = (Collection<Workflow>) workflowrepository.findAll();
+            
+            // Set headers
+            table.setHeaders(new String[] { "Workflow Id", "Name" });
+
+            // Keep adding rows to the table
+            for(Workflow workflow:workflows) {
+                table.addRow(workflow.getId().toString(), workflow.getName().toString());
+            }
+
         }
+
+        // Print table
+        table.print();
     }
     
 }
