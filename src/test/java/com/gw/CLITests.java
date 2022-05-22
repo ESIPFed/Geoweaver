62ebe9a793cb142f6000c66cfc69a0db3919d377
ZihengSun <zsun@gmu.edu>
2022-05-22 12:22:17 -0400

    some simple unit tests

--- src/test/java/com/gw/CLITests.java
+++ src/test/java/com/gw/CLITests.java
@@ -1,31 +1,52 @@
 package com.gw;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.gw.commands.TopEntryCommand;
 import com.gw.utils.BeanTool;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.boot.test.system.OutputCaptureRule;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import picocli.CommandLine;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {GeoweaverCLI.class})
+// @ExtendWith(OutputCaptureExtension.class)
 public class CLITests {
 
+    @Rule
+    public OutputCaptureRule output = new OutputCaptureRule();
+
+    @Test
+    public void testListHelp(){
+        
+        TopEntryCommand topEntryCommand = BeanTool.getBean(TopEntryCommand.class);
+        new CommandLine(topEntryCommand).execute(new String[]{"list", "--help"});
+        assertThat(output).contains("--host       list hosts");
+        assertThat(output).contains("--process    list processes");
+        assertThat(output).contains("--workflow   list workflows");
+
+    }
 
     @Test
     public void testListHost(){
         
         TopEntryCommand topEntryCommand = BeanTool.getBean(TopEntryCommand.class);
         new CommandLine(topEntryCommand).execute(new String[]{"list", "--host"});
-        //check if the function is called or the result table columns are there
-
-        // System.out.println("captured: "+output.getAll());
-        // clr.run("list --host");
-        //pass the context otherwise it will be lost after new CommandLine
+        assertThat(output).contains("Host Id");
+        assertThat(output).contains("Hostname");
+        assertThat(output).contains("IP");
+        assertThat(output).contains("Port");
 
     }
-    
+
 }
