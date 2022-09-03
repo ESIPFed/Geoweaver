220d3c97cd2220ca3e5d00b2c9302b09cb875833
Ziheng Sun <zsun@gmu.edu>
2022-09-03 00:21:37 -0400

    correct runcommand

--- src/main/java/com/gw/commands/RunCommand.java
+++ src/main/java/com/gw/commands/RunCommand.java
@@ -9,17 +9,7 @@ import picocli.CommandLine.Command;
 public class RunCommand implements Runnable {
 
     public void run() {
-        System.out.println("running run command with workflow id" + workflowId);
-        // TODO: implement
-        if (workflowZipOrPathToJson != null) {
-            System.out.println("workflow zip or path to json: " + workflowZipOrPathToJson);
-        }
-        BaseTool bt = new BaseTool();
-        WorkflowTool wt = new WorkflowTool();
-
-        if (BaseTool.isNull(historyId)) {
-            historyId = new RandomString(18).nextString();
-        }
+        
     }
     
 }
