4395f80cf25958c07730689ba04bf7c80b0c2849
Ziheng Sun <zsun@gmu.edu>
2023-03-19 14:58:57 -0400

    fix the history in prompt panel

--- src/main/resources/static/js/gw.process.sidepanel.js
+++ src/main/resources/static/js/gw.process.sidepanel.js
@@ -83,6 +83,106 @@ GW.process.sidepanel = {
 		
 	},
 
+    showHistoryDetails: function(history_id){
+		
+		console.log("history id: " + history_id);
+
+		$.ajax({
+			
+			url: "log",
+			
+			method: "POST",
+			
+			data: "type=process&id=" + history_id
+			
+		}).done(function(msg){
+
+			console.log("Log Message: " + msg);
+			
+			if(msg==""){
+				
+				alert("Cannot find the process history in the database.");
+				
+				return;
+				
+			}
+			
+			msg = GW.general.parseResponse(msg);
+
+			msg.code = msg.input;
+			
+			GW.process.sidepanel.display(msg);
+			
+			GW.process.sidepanel.displayOutput(msg);
+			
+		}).fail(function(jxr, status){
+			
+			console.error("Fail to get log.");
+		});
+		
+		
+	},
+
+    displayOutput: function(msg){
+		
+		var output = GW.general.escapeCodeforHTML(msg.output);
+		
+		if(msg.output=="logfile"){
+			
+			output = "<div class=\"spinner-border\" role=\"status\"> "+
+			"	  <span class=\"sr-only\">Loading...</span> "+
+			"	</div>";
+			
+		}
+		
+		console.log("Update the code with the old version")
+		
+		if(GW.process.sidepanel.editor){
+
+			GW.process.sidepanel.editor.setValue(GW.process.unescape(msg.input));
+
+			GW.process.util.refreshCodeEditor();
+
+		}
+		
+		output = "<p> Execution started at " + msg.begin_time + "</p>"+ 
+		
+		"<p> Execution ended at " + msg.end_time + "</p>"+
+		
+		"<p> The old code used has been refreshed in the code editor.</p>"+
+		
+		"<div>" + 
+		
+		output + "</div>";
+		
+		// $("#console-output").html(output);
+		$("#prompt-panel-process-log-window").html(output);
+		
+		$("#retrieve-result").click(function(){
+			
+			GW.result.showDialog(history_id);
+			
+		});
+		
+		if(msg.output=="logfile"){
+			
+			$.get("../temp/" + msg.id + ".log" ).success(function(data){
+				
+				if(data!=null)
+					$("#log-output").text(data);
+				else
+					$("#log-output").text("missing log");
+				
+			}).error(function(){
+				
+				$("#log-output").text("missing log");
+				
+			});
+			
+		}
+
+	},
+
 
 	showProcessLog: function(workflow_history_id, process_id, process_title){
 
@@ -108,7 +208,7 @@ GW.process.sidepanel = {
         
                     msgout = msg.history_output.replaceAll("\n", "<br/>");
     
-                    $("#prompt-process-process-log-window").append(msgout);
+                    $("#prompt-panel-process-log-window").append(msgout);
     
                 }else{
                     
@@ -170,15 +270,15 @@ GW.process.sidepanel = {
 
 		}
 
-        $('#prompt-process-panel').addClass('cd-panel--is-visible');
+        $('#prompt-panel').addClass('cd-panel--is-visible');
 
         $("#prompt-panel-main").html("");
 
         // add process code and history combo
-        let process_code_history_content = `<div id="prompt-process-editor-history-tab-panel" style="height:100%; width:100%; margin:0; padding: 0; background-color: white;">
+        let process_code_history_content = `<div id="prompt-panel-editor-history-tab-panel" style="height:100%; width:100%; margin:0; padding: 0; background-color: white;">
             <div class="subtab tab titleshadow" style="margin-top: 0; max-width: 100%">
-                <button class="tablinks-process" id="prompt-process-main-process-info-code-tab" onclick="GW.process.openCity(event, 'prompt-process-main-process-info-code')">Code</button>
-                <button class="tablinks-process" id="prompt-process-main-process-info-history-tab" onclick="GW.process.openCity(event, 'prompt-process-main-process-info-history'); GW.process.sidepanel.history('`+this.current_process_id+`', '` + this.current_process_name+`')">History</button>
+                <button class="tablinks-process" id="prompt-panel-main-process-info-code-tab" onclick="GW.process.openCity(event, 'prompt-panel-main-process-info-code')">Code</button>
+                <button class="tablinks-process" id="prompt-panel-main-process-info-history-tab" onclick="GW.process.openCity(event, 'prompt-panel-main-process-info-history'); GW.process.sidepanel.history('`+this.current_process_id+`', '` + this.current_process_name+`')">History</button>
 
                 <!-- TODO: play button, save button, full screen button-->
                 <!--
@@ -199,17 +299,17 @@ GW.process.sidepanel = {
                 
             </div>
 
-            <div id="prompt-process-main-process-info-code" class="tabcontent-process generalshadow" style="height:100%;left:0; margin:0; padding: 0; ">
-                <div class="code__container" style="font-size: 12px; margin:0; height:100%;" id="prompt-process-code-history-section">
-                    <div id="prompt-process-process_code_window" class="container__left" style="height:100%; padding:0; scrollbar-color: rgb(28, 28, 28);" >
-                        <div class="col col-md-6" id="prompt-process-code-embed" style="width:100%; margin-top:5px; padding: 0px; margin: 0px; height: calc(100%-50px);" ></div>
+            <div id="prompt-panel-main-process-info-code" class="tabcontent-process generalshadow" style="height:100%;left:0; margin:0; padding: 0; ">
+                <div class="code__container" style="font-size: 12px; margin:0; height:100%;" id="prompt-panel-code-history-section">
+                    <div id="prompt-panel-process_code_window" class="container__left" style="height:100%; padding:0; scrollbar-color: rgb(28, 28, 28);" >
+                        <div class="col col-md-6" id="prompt-panel-code-embed" style="width:100%; margin-top:5px; padding: 0px; margin: 0px; height: calc(100%-50px);" ></div>
                     </div>
-                    <div class="resizer" id="prompt-process-dragMe"></div>
-                    <div id="prompt-process-single-console-content" class="container__right" style="height:100%; overflow-y: scroll; scrollbar-color: rgb(28, 28, 28); background-color: rgb(28, 28, 28); color: white;">
+                    <div class="resizer" id="prompt-panel-dragMe"></div>
+                    <div id="prompt-panel-single-console-content" class="container__right" style="height:100%; overflow-y: scroll; scrollbar-color: rgb(28, 28, 28); background-color: rgb(28, 28, 28); color: white;">
                         <h4>Logging</h4>
-                        <div id="prompt-process-process-log-window" style="overflow-wrap: break-word;"> </div>
+                        <div id="prompt-panel-process-log-window" style="overflow-wrap: break-word;"> </div>
                         <div class="row" style="padding:0px; margin:0px;" >
-                            <div class="col col-md-12" id="prompt-process-console-output"  style="width:100%; padding:0px; margin:0px; height:calc(100%-50px); " >
+                            <div class="col col-md-12" id="prompt-panel-console-output"  style="width:100%; padding:0px; margin:0px; height:calc(100%-50px); " >
                                 <div class="d-flex justify-content-center"><div class="dot-flashing invisible"></div></div>
                             </div>
                         </div>
@@ -217,25 +317,25 @@ GW.process.sidepanel = {
                 </div>
             </div>
 
-            <div id="prompt-process-main-process-info-history" class="tabcontent-process generalshadow" style="height:100%; overflow-y: scroll; left:0; margin:0; padding: 0; display:none;">
-                <div class="row" id="prompt-process-process-history-container" style="padding:0px; color:white; margin:0px; background-color:rgb(28, 28, 28);" >
+            <div id="prompt-panel-main-process-info-history" class="tabcontent-process generalshadow" style="height:100%; overflow-y: scroll; left:0; margin:0; padding: 0; display:none;">
+                <div class="row" id="prompt-panel-process-history-container" style="padding:0px; color:white; margin:0px; background-color:rgb(28, 28, 28);" >
                 </div>
             </div>
 
-            <div id="prompt-process-execution_context"></div>
+            <div id="prompt-panel-execution_context"></div>
         </div>`
         $("#prompt-panel-main").append(process_code_history_content)
 
         // fill in values
-        $("#prompt-process-processcategory").val(code_type);
+        $("#prompt-panel-processcategory").val(code_type);
 		
-		$("#prompt-process-processname").val(this.current_process_name);
+		$("#prompt-panel-processname").val(this.current_process_name);
 		
-		$("#prompt-process-processid").val(this.current_process_id);
+		$("#prompt-panel-processid").val(this.current_process_id);
 
-        $("#prompt-process-main-process-info-code").hide().fadeIn('fast'); // refresh to make height full
+        $("#prompt-panel-main-process-info-code").hide().fadeIn('fast'); // refresh to make height full
 		
-		GW.process.sidepanel.editor = GW.process.util.displayCodeArea(code_type,  code, "#prompt-process-code-embed", "#prompt-process-process_code_window");
+		GW.process.sidepanel.editor = GW.process.util.displayCodeArea(code_type,  code, "#prompt-panel-code-embed", "#prompt-panel-process_code_window");
 		
 		// GW.process.util.displayToolbar(process_id, process_name, code_type, );
 
@@ -318,42 +418,42 @@ GW.process.sidepanel = {
 
     switchFullScreen: function(){
 
-        GW.editor.switchFullScreenUtil('#prompt-process-editor-history-tab-panel', '#prompt-process-main-process-info-code', '#prompt-process-main-process-info-history')
+        GW.editor.switchFullScreenUtil('#prompt-panel-editor-history-tab-panel', '#prompt-panel-main-process-info-code', '#prompt-panel-main-process-info-history')
 
     },
 
     leftDock: function(){
 
-        GW.process.util.leftDock("prompt-process-code-history-section", "prompt-process-process_code_window", 
-            "prompt-process-single-console-content", "prompt-process-dragMe")
+        GW.process.util.leftDock("prompt-panel-code-history-section", "prompt-panel-process_code_window", 
+            "prompt-panel-single-console-content", "prompt-panel-dragMe")
         GW.process.sidepanel.dockmode = "left";
 
     },
 
     bottomDock: function(){
 
-        GW.process.util.bottomDock("prompt-process-code-history-section", "prompt-process-process_code_window", 
-            "prompt-process-single-console-content", "prompt-process-dragMe")
+        GW.process.util.bottomDock("prompt-panel-code-history-section", "prompt-panel-process_code_window", 
+            "prompt-panel-single-console-content", "prompt-panel-dragMe")
         GW.process.sidepanel.dockmode = "bottom";
 
     },
 
     history: function(process_id, process_name){
 
-        GW.process.util.history(process_id, "#prompt-process-process-history-container", 'process_history_table', 
-			"#closeHistory", "prompt-process-main-process-info-history-tab", "prompt-process-main-process-info-history")
+        GW.process.util.history(process_id, "#prompt-panel-process-history-container", 'process_history_table', 
+			"#closeHistory", "prompt-panel-main-process-info-history-tab", "prompt-panel-main-process-info-history")
 
     },
 
     close: function(){
 
-        $('#prompt-process-panel').removeClass('cd-panel--is-visible');
+        $('#prompt-panel').removeClass('cd-panel--is-visible');
 
     },
 
     isPresent: function(){
 
-        return $('#prompt-process-panel').hasClass('cd-panel--is-visible')
+        return $('#prompt-panel').hasClass('cd-panel--is-visible')
 
     },
 
