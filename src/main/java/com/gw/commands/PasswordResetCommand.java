f3c4ffa9099b2a8f977342cd20ca9f0f8ca3be5a
Ziheng Sun <zsun@gmu.edu>
2023-06-14 12:49:59 -0400

    allow reset password from terminal

--- src/main/java/com/gw/commands/PasswordResetCommand.java
+++ src/main/java/com/gw/commands/PasswordResetCommand.java
@@ -8,38 +8,56 @@ import com.gw.utils.BaseTool;
 import org.springframework.stereotype.Component;
 
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 @Command(name = "resetpassword", description = "Reset password of local host")
 @Component
 public class PasswordResetCommand implements Runnable {
 
+    @Option(names = { "-p", "--password" }, description = "the new password")
+    String new_password;
+
     public void run() {
-        Console console = System.console();
-        if (console == null) {
-            System.out.println("Couldn't get Console instance");
-            System.exit(0);
-        }
 
-        console.printf("Reset Geoweaver Localhost password%n");
-        char[] passwordArray = console.readPassword("Enter password: ");
-        // console.printf("Password entered was: %s%n", new String(passwordArray));
-        char[] secondpasswordArray = console.readPassword("Retype password: ");
+        BaseTool bt = new BaseTool();
 
-        if(Arrays.equals(passwordArray, secondpasswordArray)){
+        if(new_password != null){
 
-            String originalpassword = new String(passwordArray);
-        
-            BaseTool bt = new BaseTool();
-
-            bt.setLocalhostPassword(originalpassword, true);
+            bt.setLocalhostPassword(new_password, true);
 
             System.out.println("NOTE: Password updated.");
 
         }else{
 
-            System.err.println("ERROR: The two entered passwords don't match.");
+            Console console = System.console();
+            if (console == null) {
+                System.out.println("Couldn't get Console instance");
+                System.exit(0);
+            }
+
+
+            console.printf("Reset Geoweaver Localhost password%n");
+            char[] passwordArray = console.readPassword("Enter password: ");
+            // console.printf("Password entered was: %s%n", new String(passwordArray));
+            char[] secondpasswordArray = console.readPassword("Retype password: ");
+
+            if(Arrays.equals(passwordArray, secondpasswordArray)){
+
+                String originalpassword = new String(passwordArray);
+
+                bt.setLocalhostPassword(originalpassword, true);
+
+                System.out.println("NOTE: Password updated.");
+
+            }else{
+
+                System.err.println("ERROR: The two entered passwords don't match.");
+
+            }
 
         }
+
+        
 
     }
 }
