3e7707053b95df16654157b093d53db068809bd3
ZihengSun <zsun@gmu.edu>
2024-02-14 02:23:20 -0500

    messed up the previous version, try again

--- src/main/resources/static/js/gw.js
+++ src/main/resources/static/js/gw.js
@@ -9,7 +9,7 @@ edu = {
 							
 					sponsor: "ESIPLab incubator project, NASA ACCESS project, NSF Geoinformatics project, NSF Cybertraining project",
 					
-					version: "1.3.5",
+					version: "1.3.6",
 					
 					author: "open source contributors",
 					
