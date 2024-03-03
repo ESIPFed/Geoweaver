4715373393c4534d053bc27992b93df7cbc4c336
Ziheng Sun <zsun@gmu.edu>
2024-03-03 14:09:42 -0500

    bump version

--- src/main/resources/static/js/gw.js
+++ src/main/resources/static/js/gw.js
@@ -9,7 +9,7 @@ edu = {
 							
 					sponsor: "ESIPLab incubator project, NASA ACCESS project, NSF Geoinformatics project, NSF Cybertraining project",
 					
-					version: "1.3.8",
+					version: "1.3.9",
 					
 					author: "open source contributors",
 					
