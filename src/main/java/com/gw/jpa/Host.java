5d2cc02b96020ab05af95e221267f38a1f37ee66
Gokul Prathin A <gokulprathin8@gmail.com>
2024-03-24 00:03:06 -0400

    * cleanup lombok

--- src/main/java/com/gw/jpa/Host.java
+++ src/main/java/com/gw/jpa/Host.java
@@ -1,5 +1,9 @@
 package com.gw.jpa;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -9,102 +13,23 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Host {
 
 	@Id
-	String id;
-	
-	String name, ip, port, username, owner, type, url;
+	private String id;
 
-	String confidential;
+	private String name;
+	private String ip;
+	private String port;
+	private String username;
+	private String owner;
+	private String type;
+	private String url;
+	private String confidential;
 
-	@OneToMany(cascade = CascadeType.ALL,
-		fetch = FetchType.LAZY,
-		mappedBy="hostobj")
-    private Set<Environment> envs;
-
-	public Set<Environment> getEnvs() {
-		return this.envs;
-	}
-
-	public void setEnvs(Set<Environment> envs) {
-		this.envs = envs;
-	}
-
-	public String getConfidential() {
-		return this.confidential;
-	}
-
-	public void setConfidential(String confidential) {
-		this.confidential = confidential;
-	}
-
-	
-	
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getIp() {
-		return ip;
-	}
-
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
-
-	public String getPort() {
-		return port;
-	}
-
-	public void setPort(String port) {
-		this.port = port;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getOwner() {
-		return owner;
-	}
-
-	public void setOwner(String owner) {
-		this.owner = owner;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	
-	
-	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "hostobj")
+	private Set<Environment> envs;
 }
