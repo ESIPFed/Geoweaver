c6b93c9c1f29036a1a96f9c95275553913632cd1
Gokul Prathin A <gokulprathin8@gmail.com>
2024-03-24 02:10:25 -0400

    merged master + resolved merge conflicts

--- src/main/java/com/gw/jpa/GWProcess.java
+++ src/main/java/com/gw/jpa/GWProcess.java
@@ -1,5 +1,9 @@
 package com.gw.jpa;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.Lob;
 import javax.persistence.Entity;
