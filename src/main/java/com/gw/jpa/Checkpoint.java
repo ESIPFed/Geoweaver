9ff5fafa99d0bc56fba0ea06ff59308022e0955c
Gokul Prathin A <gokulprathin8@gmail.com>
2024-03-23 23:33:59 -0400

    * cleanup Checkpoint.java

--- src/main/java/com/gw/jpa/Checkpoint.java
+++ src/main/java/com/gw/jpa/Checkpoint.java
@@ -12,6 +12,10 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
@@ -20,16 +24,19 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "WorkflowCheckpoint")
+@Getter
+@Setter
+@NoArgsConstructor // Lombok annotations
 public class Checkpoint {
 
     @Id
     @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name="uuid2", strategy = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Type(type = "uuid-char")
     @Column(name = "id", columnDefinition = "VARCHAR(36)")
     private UUID id;
 
-    @Column(name="executionId")
+    @Column(name = "executionId")
     private String executionId;
 
     @Lob
@@ -48,58 +55,9 @@ public class Checkpoint {
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt;
 
-
-    public String getExecutionId() {
-        return executionId;
-    }
-
-    public void setExecutionId(String executionId) {
-        this.executionId = executionId;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
     @PrePersist
     protected void onCreate() {
         createdAt = new Date();
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getEdges() {
-        return edges;
-    }
-
-    public void setEdges(String edges) {
-        this.edges = edges;
-    }
-
-    public String getNodes() {
-        return nodes;
-    }
-
-    public void setNodes(String nodes) {
-        this.nodes = nodes;
-    }
-
-    public Workflow getWorkflow() {
-        return workflow;
-    }
-
-    public void setWorkflow(Workflow workflow) {
-        this.workflow = workflow;
     }
 
 }
