081aa2444995d7c981900fb2dc247c2a1ab77537
iammeghana <mkoramut@gmu.edu>
2024-05-13 15:35:30 -0400

    pswd check

--- cypress/cypress/e2e/spec.cy.js
+++ cypress/cypress/e2e/spec.cy.js
@@ -192,7 +192,7 @@ describe('Navigation', () => {
 
 
 describe('Host Testing', () => {
-  it('should submit a create new host form successfpully', () => {
+  it('should submit a create new host form successfully', () => {
         cy.visit('http://localhost:8070/Geoweaver'); 
         cy.get('.introjs-skipbutton').click();
         cy.get('#newhost').click();
@@ -355,14 +355,10 @@ describe('Write Password into .secret', () => {
     cy.writeFile(filePath, dataToWrite, 'binary')
       .then(() => {
         cy.readFile(filePath).should('contain', dataToWrite);
-
-
         cy.readFile(filePath).then((fileContents) => {
           cy.task('log', 'File Contents: ' + fileContents);
           cy.log('File Contents:', fileContents);
         });
-
-        
       })
   });
 });
@@ -388,7 +384,7 @@ describe('Create Python process and run it', () => {
       const selector = `[onclick="GW.process.runProcess('${processId}', 'hello_world.py', 'python')"]`;
       cy.get(selector).click(); // Perform actions using the dynamically constructed selector
     });
-
+    cy.intercept('POST', '/Geoweaver/web/executeProcess').as('executeProcess'); // Intercept the POST request
     cy.get('#host-execute-btn').click();
 
     cy.get('#process-confirm-btn').click();
@@ -400,24 +396,7 @@ describe('Create Python process and run it', () => {
     cy.get('#single-console-content').should('contain', 'Exit Code: 0');
     cy.get('#process-log-window').click();
     cy.get('#process-log-window').should('be.visible');
-    // cy.get('#process-log-window').click();
-    // cy.get('#process-log-window > :nth-child(3) > span').click();
-    // cy.get('#single-console-content').click();
-    // cy.get('#process-log-window > :nth-child(3) > span').should('have.text', 'hello world!');
-    // cy.get('#process-log-window').click();
-    // cy.get('#process-log-window > :nth-child(4)').click();
-    // cy.get('#process-log-window > :nth-child(4) > span').should('have.text', 'Exit Code: 0');
-    // /* ==== Generated with Cypress Studio ==== */
-    // cy.get('#process-log-window').click();
-    // cy.get('#single-console-content').click();
-    // cy.get('#process-log-window > :nth-child(3) > span').should('have.text', 'hello world!');
-    // cy.get('#single-console-content').click();
-    // cy.get('#process-log-window > :nth-child(4) > span').should('have.text', 'Exit Code: 0');
-
-  // });
-  
-
-    /* ==== End Cypress Studio ==== */
+    cy.wait('@executeProcess').its('response.statusCode').should('eq', 200);
   });
 });
 
@@ -474,8 +453,6 @@ describe('Hosts Testing', () => {
     cy.wait('@readEnvironment').then((interception) => {
       expect(interception.response.statusCode).to.equal(200);
     });
-    cy.get('#environment-iframe').should('be.visible'); 
-
   })
 
   it('LocalHost File Upload', () => {
@@ -488,8 +465,8 @@ describe('Hosts Testing', () => {
     cy.get('#inputpswd').type('1234');
     cy.get('#pswd-confirm-btn').click();
     cy.get('#host-file-uploader').click();
-    cy.get('.text-info').click();
-    cy.get('.text-info').should('be.visible');
+    cy.intercept('POST', 'http://localhost:8070/Geoweaver/web/authenticateUser').as('authenticateUser');
+    cy.wait('@authenticateUser').its('response.statusCode').should('eq', 200);
   })
 
 });
