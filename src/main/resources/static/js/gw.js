98155ffdedb811016850ec10ae04d0ea463cc0d6
iammeghana <mkoramut@gmu.edu>
2024-06-30 22:29:19 -0400

    version bump to 1.6.1

--- src/main/resources/static/js/gw.js
+++ src/main/resources/static/js/gw.js
@@ -9,7 +9,7 @@ edu = {
         sponsor:
           "ESIPLab incubator project, NASA ACCESS project, NSF Geoinformatics project, NSF Cybertraining project",
 
-        version: "1.6.0",
+        version: "1.6.1",
 
         author: "open source contributors",
 
