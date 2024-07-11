cc6388f4e3d9b0bdbbba472eabc42219eca87331
saivivek mac <saivivek116@gmail.com>
2024-07-10 20:05:31 -0700

    new pom.xml changes from scratch but it has lot of issues when tried to run the application

--- src/main/java/com/gw/server/FileUploadServlet.java
+++ src/main/java/com/gw/server/FileUploadServlet.java
@@ -12,10 +12,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.RequestContext;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
+//import org.apache.commons.fileupload.FileItem;
+//import org.apache.commons.fileupload.RequestContext;
+//import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+//import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,201 +27,201 @@ import org.springframework.beans.factory.annotation.Value;
     urlPatterns = {"/FileUploadServlet"})
 public class FileUploadServlet extends HttpServlet {
 
-  private String relativePath = null,
-      filePath = null,
-      tempPath = null,
-      prefix_url = null,
-      callback = null;
-
-  private int maxvol = 2000;
-
-  Logger logger = LoggerFactory.getLogger(getClass());
-
-  @Value("${geoweaver.upload_file_path}")
-  String upload_file_path;
-
-  @Value("${geoweaver.temp_file_path}")
-  String temp_file_path;
-
-  @Value("${geoweaver.workspace}")
-  String workspace;
-
-  @Autowired BaseTool bt;
-
-  @Override
-  public void init(ServletConfig config) throws ServletException {
-
-    //    	super.init(config);
-
-    //        relativePath = config.getInitParameter("filepath");
-    //
-    //        tempPath = config.getInitParameter("temppath");
-
-    relativePath = upload_file_path;
-
-    tempPath = temp_file_path;
-
-    //        filePath = bt.getWebAppRootPath() + relativePath;
-    //
-    //        tempPath = bt.getWebAppRootPath() + tempPath;
-
-    filePath = bt.getFileTransferFolder();
-
-    tempPath = bt.normalizedPath(workspace) + "/" + tempPath;
-
-    File uploadfolder = new File(filePath);
-
-    File tempfolder = new File(tempPath);
-
-    if (!uploadfolder.exists()) {
-
-      uploadfolder.mkdirs();
-    }
-
-    if (!tempfolder.exists()) {
-
-      tempfolder.mkdirs();
-    }
-  }
-
-  /**
-   * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-   *
-   * @param req servlet request
-   * @param res servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  protected void processRequest(HttpServletRequest req, HttpServletResponse res)
-      throws ServletException, IOException {
-    res.setContentType("text/html; charset=utf-8");
-    PrintWriter pw = res.getWriter();
-    try {
-      // initialize the prefix url
-      if (prefix_url == null) {
-
-        prefix_url = "download/" + relativePath + "/";
-      }
-
-      DiskFileItemFactory diskFactory = new DiskFileItemFactory();
-      // threshold  4M
-      // extend to 2GB - updated by ziheng - 7/5/2018
-      diskFactory.setSizeThreshold(maxvol * 1024);
-      // repository
-      logger.debug("Temp file path: " + tempPath);
-      File newrepo = new File(tempPath);
-      diskFactory.setRepository(newrepo);
-
-      ServletFileUpload upload = new ServletFileUpload(diskFactory);
-      // 2000M
-      //            upload.setSizeMax(maxvol * 1024 * 1024);
-      // HTTP
-      List fileItems = null;//upload.parseRequest((RequestContext) req);
-      Iterator iter = fileItems.iterator();
-      while (iter.hasNext()) {
-        FileItem item = (FileItem) iter.next();
-        if (item.isFormField()) {
-          processFormField(item, pw);
-        } else {
-          processUploadFile(item, pw);
-        }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-      pw.println("ERR:" + e.getClass().getName() + ":" + e.getLocalizedMessage());
-    } finally {
-      pw.flush();
-      pw.close();
-    }
-  }
-
-  /**
-   * Information of the fields except file fields
-   *
-   * @param item
-   * @param pw
-   * @throws Exception
-   */
-  private void processFormField(FileItem item, PrintWriter pw) throws Exception {
-    String name = item.getFieldName();
-    String value = item.getString();
-    logger.debug(name + " : " + value + "\r\n");
-    if (name.equals("script")) {
-      pw.println("<script>");
-      pw.println(value);
-      pw.println("</script>");
-    }
-    //        pw.println(name + " : " + value + "\r\n");
-  }
-
-  /**
-   * Process uploaded file
-   *
-   * @param item
-   * @param pw
-   * @throws Exception
-   */
-  private void processUploadFile(FileItem item, PrintWriter pw) throws Exception {
-    String filename = item.getName();
-
-    int index = filename.lastIndexOf("\\");
-
-    filename = filename.substring(index + 1, filename.length());
-
-    long fileSize = item.getSize();
-
-    if ("".equals(filename) && fileSize == 0) {
-      throw new RuntimeException("You didn't upload a file.");
-      // return;
-    }
-
-    File uploadFile = new File(filePath + "/" + filename);
-
-    item.write(uploadFile);
-
-    logger.debug("prefix url : " + prefix_url);
-
-    logger.info("file name : " + filename);
-
-    //        pw.println("<a id=\"filelink\" href=\""+prefix_url+filename+"\" >Link</a> to the
-    // uploaded file : "+filename);
-    pw.print("{ \"url\": \"" + prefix_url + filename + "\", \"filename\": \"" + filename + "\" }");
-
-    logger.debug(fileSize + "\r\n");
-  }
-
-  /**
-   * Handles the HTTP <code>GET</code> method.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-
-    PrintWriter pw = response.getWriter();
-
-    pw.println("wrong way");
-
-    pw.flush();
-  }
-
-  /**
-   * Handles the HTTP <code>POST</code> method.
-   *
-   * @param request servlet request
-   * @param response servlet response
-   * @throws ServletException if a servlet-specific error occurs
-   * @throws IOException if an I/O error occurs
-   */
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    processRequest(request, response);
-  }
+//  private String relativePath = null,
+//      filePath = null,
+//      tempPath = null,
+//      prefix_url = null,
+//      callback = null;
+//
+//  private int maxvol = 2000;
+//
+//  Logger logger = LoggerFactory.getLogger(getClass());
+//
+//  @Value("${geoweaver.upload_file_path}")
+//  String upload_file_path;
+//
+//  @Value("${geoweaver.temp_file_path}")
+//  String temp_file_path;
+//
+//  @Value("${geoweaver.workspace}")
+//  String workspace;
+//
+//  @Autowired BaseTool bt;
+//
+//  @Override
+//  public void init(ServletConfig config) throws ServletException {
+//
+//    //    	super.init(config);
+//
+//    //        relativePath = config.getInitParameter("filepath");
+//    //
+//    //        tempPath = config.getInitParameter("temppath");
+//
+//    relativePath = upload_file_path;
+//
+//    tempPath = temp_file_path;
+//
+//    //        filePath = bt.getWebAppRootPath() + relativePath;
+//    //
+//    //        tempPath = bt.getWebAppRootPath() + tempPath;
+//
+//    filePath = bt.getFileTransferFolder();
+//
+//    tempPath = bt.normalizedPath(workspace) + "/" + tempPath;
+//
+//    File uploadfolder = new File(filePath);
+//
+//    File tempfolder = new File(tempPath);
+//
+//    if (!uploadfolder.exists()) {
+//
+//      uploadfolder.mkdirs();
+//    }
+//
+//    if (!tempfolder.exists()) {
+//
+//      tempfolder.mkdirs();
+//    }
+//  }
+//
+//  /**
+//   * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+//   *
+//   * @param req servlet request
+//   * @param res servlet response
+//   * @throws ServletException if a servlet-specific error occurs
+//   * @throws IOException if an I/O error occurs
+//   */
+//  protected void processRequest(HttpServletRequest req, HttpServletResponse res)
+//      throws ServletException, IOException {
+//    res.setContentType("text/html; charset=utf-8");
+//    PrintWriter pw = res.getWriter();
+//    try {
+//      // initialize the prefix url
+//      if (prefix_url == null) {
+//
+//        prefix_url = "download/" + relativePath + "/";
+//      }
+//
+//      DiskFileItemFactory diskFactory = new DiskFileItemFactory();
+//      // threshold  4M
+//      // extend to 2GB - updated by ziheng - 7/5/2018
+//      diskFactory.setSizeThreshold(maxvol * 1024);
+//      // repository
+//      logger.debug("Temp file path: " + tempPath);
+//      File newrepo = new File(tempPath);
+//      diskFactory.setRepository(newrepo);
+//
+//      ServletFileUpload upload = new ServletFileUpload(diskFactory);
+//      // 2000M
+//      //            upload.setSizeMax(maxvol * 1024 * 1024);
+//      // HTTP
+//      List fileItems = null;//upload.parseRequest((RequestContext) req);
+//      Iterator iter = fileItems.iterator();
+//      while (iter.hasNext()) {
+//        FileItem item = (FileItem) iter.next();
+//        if (item.isFormField()) {
+//          processFormField(item, pw);
+//        } else {
+//          processUploadFile(item, pw);
+//        }
+//      }
+//    } catch (Exception e) {
+//      e.printStackTrace();
+//      pw.println("ERR:" + e.getClass().getName() + ":" + e.getLocalizedMessage());
+//    } finally {
+//      pw.flush();
+//      pw.close();
+//    }
+//  }
+//
+//  /**
+//   * Information of the fields except file fields
+//   *
+//   * @param item
+//   * @param pw
+//   * @throws Exception
+//   */
+//  private void processFormField(FileItem item, PrintWriter pw) throws Exception {
+//    String name = item.getFieldName();
+//    String value = item.getString();
+//    logger.debug(name + " : " + value + "\r\n");
+//    if (name.equals("script")) {
+//      pw.println("<script>");
+//      pw.println(value);
+//      pw.println("</script>");
+//    }
+//    //        pw.println(name + " : " + value + "\r\n");
+//  }
+//
+//  /**
+//   * Process uploaded file
+//   *
+//   * @param item
+//   * @param pw
+//   * @throws Exception
+//   */
+//  private void processUploadFile(FileItem item, PrintWriter pw) throws Exception {
+//    String filename = item.getName();
+//
+//    int index = filename.lastIndexOf("\\");
+//
+//    filename = filename.substring(index + 1, filename.length());
+//
+//    long fileSize = item.getSize();
+//
+//    if ("".equals(filename) && fileSize == 0) {
+//      throw new RuntimeException("You didn't upload a file.");
+//      // return;
+//    }
+//
+//    File uploadFile = new File(filePath + "/" + filename);
+//
+//    item.write(uploadFile);
+//
+//    logger.debug("prefix url : " + prefix_url);
+//
+//    logger.info("file name : " + filename);
+//
+//    //        pw.println("<a id=\"filelink\" href=\""+prefix_url+filename+"\" >Link</a> to the
+//    // uploaded file : "+filename);
+//    pw.print("{ \"url\": \"" + prefix_url + filename + "\", \"filename\": \"" + filename + "\" }");
+//
+//    logger.debug(fileSize + "\r\n");
+//  }
+//
+//  /**
+//   * Handles the HTTP <code>GET</code> method.
+//   *
+//   * @param request servlet request
+//   * @param response servlet response
+//   * @throws ServletException if a servlet-specific error occurs
+//   * @throws IOException if an I/O error occurs
+//   */
+//  @Override
+//  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+//      throws ServletException, IOException {
+//
+//    PrintWriter pw = response.getWriter();
+//
+//    pw.println("wrong way");
+//
+//    pw.flush();
+//  }
+//
+//  /**
+//   * Handles the HTTP <code>POST</code> method.
+//   *
+//   * @param request servlet request
+//   * @param response servlet response
+//   * @throws ServletException if a servlet-specific error occurs
+//   * @throws IOException if an I/O error occurs
+//   */
+//  @Override
+//  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//      throws ServletException, IOException {
+//    processRequest(request, response);
+//  }
 
   /**
    * Returns a short description of the servlet.
--- src/test/java/com/gw/ServletTest.java
+++ src/test/java/com/gw/ServletTest.java
@@ -1,196 +1,196 @@
-package com.gw;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Map;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.websocket.EndpointConfig;
-import jakarta.websocket.Session;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gw.ssh.SSHSession;
-import com.gw.server.FileUploadServlet;
-import com.gw.server.TerminalServlet;
-import com.gw.server.TestSocketServlet;
-import com.gw.server.WorkflowServlet;
-import com.gw.tools.UserTool;
-import com.gw.utils.BaseTool;
-
-import org.apache.commons.fileupload.FileItem;
-import org.apache.log4j.Logger;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.test.util.ReflectionTestUtils;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = GeoweaverApplication.class)
-public class ServletTest extends AbstractHelperMethodsTest {
-
-    @Autowired
-    UserTool ut;
-
-    @Autowired
-    BaseTool bt;
-
-    @Autowired
-    private TestRestTemplate testrestTemplate;
-
-    @Autowired
-    TerminalServlet servlet;
-
-    @Autowired
-    WorkflowServlet wfServlet;
-
-    FileUploadServlet fileServlet;
-
-    @LocalServerPort
-    private int port;
-
-    Logger logger = Logger.getLogger(this.getClass());
-
-    @Test
-    @DisplayName("Test Terminal OnOpen Websocket Message")
-    void testTerminalOnOpen() {
-
-        Session session = Mockito.mock(Session.class);
-
-        servlet.open(session, null);
-
-    }
-
-    @Test
-    @DisplayName("Test Terminal OnClose Websocket Message")
-    void testTerminalOnClose() {
-
-        Session session = Mockito.mock(Session.class);
-        servlet.close(session);
-
-    }
-
-    @Test
-    @DisplayName("Test Terminal Error Websocket Message")
-    void testTerminalError() {
-
-        try {
-
-            servlet.error(null, new RuntimeException("Test Terminal Websocket Error"));
-
-        } catch (Throwable e) {
-            // TODO: handle exception
-        }
-
-    }
-
-    @Test
-    @DisplayName("Test Terminal Echo Websocket Message")
-    void testTerminalEcho() {
-
-        Session session = Mockito.mock(Session.class);
-        servlet.echo("Test Terminal Echo", session);
-
-    }
-
-    @Test
-    @DisplayName("Test Terminal Find Session")
-    void testTerminalFindBySession() {
-
-        servlet.findSessionById("");
-
-    }
-
-    @Test
-    @DisplayName("Test Workflow Socket Registration")
-    void testWorkflowRegisterSession() {
-
-        // Session session = new Session();
-        try {
-            wfServlet.registerSession(null, "");
-
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
-    }
-
-    @Test
-    @DisplayName("Test Workflow OnOpen Message")
-    void testWorkflowOnOpen() {
-
-        try {
-
-            wfServlet.open(null, null);
-
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
-    }
-
-    @Test
-    @DisplayName("Test Workflow OnClose Message")
-    void testWorkflowOnClose() {
-
-        try {
-
-            wfServlet.close(null);
-
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
-    }
-
-    @Test
-    @DisplayName("Test Workflow Error Message")
-    void testWorkflowError() {
-
-        try {
-            wfServlet.error(null, new Throwable("Test Workflow Socket Error"));
-
-        } catch (Throwable e) {
-            // TODO: handle exception
-        }
-
-    }
-
-    @Test
-    @DisplayName("Test Workflow Echo Message")
-    void testWorkflowEcho() {
-
-        try {
-
-            wfServlet.echo("Test Terminal Echo", null);
-
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
-    }
-
-    @Test
-    @DisplayName("Test Workflow findSessionByToken")
-    void testWorkflowFindSessionByToken() {
-        // Test fails with "NullPointerException"
-
-        WorkflowServlet.findSessionByToken("");
-
-    }
-
-}
+//package com.gw;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.util.Map;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import jakarta.websocket.EndpointConfig;
+//import jakarta.websocket.Session;
+//
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.JsonMappingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.gw.ssh.SSHSession;
+//import com.gw.server.FileUploadServlet;
+//import com.gw.server.TerminalServlet;
+//import com.gw.server.TestSocketServlet;
+//import com.gw.server.WorkflowServlet;
+//import com.gw.tools.UserTool;
+//import com.gw.utils.BaseTool;
+//
+//import org.apache.commons.fileupload.FileItem;
+//import org.apache.log4j.Logger;
+//import static org.junit.jupiter.api.Assertions.*;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.test.web.server.LocalServerPort;
+//import org.springframework.http.HttpEntity;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.util.ReflectionTestUtils;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = GeoweaverApplication.class)
+//public class ServletTest extends AbstractHelperMethodsTest {
+//
+//    @Autowired
+//    UserTool ut;
+//
+//    @Autowired
+//    BaseTool bt;
+//
+//    @Autowired
+//    private TestRestTemplate testrestTemplate;
+//
+//    @Autowired
+//    TerminalServlet servlet;
+//
+//    @Autowired
+//    WorkflowServlet wfServlet;
+//
+//    FileUploadServlet fileServlet;
+//
+//    @LocalServerPort
+//    private int port;
+//
+//    Logger logger = Logger.getLogger(this.getClass());
+//
+//    @Test
+//    @DisplayName("Test Terminal OnOpen Websocket Message")
+//    void testTerminalOnOpen() {
+//
+//        Session session = Mockito.mock(Session.class);
+//
+//        servlet.open(session, null);
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Test Terminal OnClose Websocket Message")
+//    void testTerminalOnClose() {
+//
+//        Session session = Mockito.mock(Session.class);
+//        servlet.close(session);
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Test Terminal Error Websocket Message")
+//    void testTerminalError() {
+//
+//        try {
+//
+//            servlet.error(null, new RuntimeException("Test Terminal Websocket Error"));
+//
+//        } catch (Throwable e) {
+//            // TODO: handle exception
+//        }
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Test Terminal Echo Websocket Message")
+//    void testTerminalEcho() {
+//
+//        Session session = Mockito.mock(Session.class);
+//        servlet.echo("Test Terminal Echo", session);
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Test Terminal Find Session")
+//    void testTerminalFindBySession() {
+//
+//        servlet.findSessionById("");
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Test Workflow Socket Registration")
+//    void testWorkflowRegisterSession() {
+//
+//        // Session session = new Session();
+//        try {
+//            wfServlet.registerSession(null, "");
+//
+//        } catch (Exception e) {
+//            // TODO: handle exception
+//        }
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Test Workflow OnOpen Message")
+//    void testWorkflowOnOpen() {
+//
+//        try {
+//
+//            wfServlet.open(null, null);
+//
+//        } catch (Exception e) {
+//            // TODO: handle exception
+//        }
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Test Workflow OnClose Message")
+//    void testWorkflowOnClose() {
+//
+//        try {
+//
+//            wfServlet.close(null);
+//
+//        } catch (Exception e) {
+//            // TODO: handle exception
+//        }
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Test Workflow Error Message")
+//    void testWorkflowError() {
+//
+//        try {
+//            wfServlet.error(null, new Throwable("Test Workflow Socket Error"));
+//
+//        } catch (Throwable e) {
+//            // TODO: handle exception
+//        }
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Test Workflow Echo Message")
+//    void testWorkflowEcho() {
+//
+//        try {
+//
+//            wfServlet.echo("Test Terminal Echo", null);
+//
+//        } catch (Exception e) {
+//            // TODO: handle exception
+//        }
+//
+//    }
+//
+//    @Test
+//    @DisplayName("Test Workflow findSessionByToken")
+//    void testWorkflowFindSessionByToken() {
+//        // Test fails with "NullPointerException"
+//
+//        WorkflowServlet.findSessionByToken("");
+//
+//    }
+//
+//}
