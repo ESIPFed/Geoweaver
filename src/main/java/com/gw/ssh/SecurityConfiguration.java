e2bb5ff431db87b277a24847d2c5155bd5a9f8b4
saivivek mac <saivivek116@gmail.com>
2024-07-11 17:03:13 -0700

    application is running but need to fix some files like GoogleEarthController.java and JupyterController.java and task manager.java

--- src/main/java/com/gw/ssh/SecurityConfiguration.java
+++ src/main/java/com/gw/ssh/SecurityConfiguration.java
@@ -1,54 +1,71 @@
-//package com.gw.ssh;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//import java.util.Arrays;
-//import org.apache.log4j.Logger;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.builders.WebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+package com.gw.ssh;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//
-//// import com.gw.tools.SessionManager;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//  protected final Logger log = Logger.getLogger(getClass());
-//
-//  @Bean
-//  public SSHAuthenticationProvider sshAuthentication() {
-//    SSHAuthenticationProvider sshAuthentication = new SSHAuthenticationProvider();
-//    return sshAuthentication;
-//  }
-//
-//  /*
-//   * TODO - use 'registerAuthentication' in 3.2.0.RC1 and 'configure' in 3.2.0.RELEASE
-//   * but note that 'configure' does not appear to work in tomcat7
-//   */
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+// import com.gw.tools.SessionManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+  protected final Logger log = Logger.getLogger(getClass());
+
+  @Bean
+  public SSHAuthenticationProvider sshAuthentication() {
+    SSHAuthenticationProvider sshAuthentication = new SSHAuthenticationProvider();
+    return sshAuthentication;
+  }
+
+  /*
+   * TODO - use 'registerAuthentication' in 3.2.0.RC1 and 'configure' in 3.2.0.RELEASE
+   * but note that 'configure' does not appear to work in tomcat7
+   */
 //  /*~~(Migrate manually based on https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter)~~>*/@Override
 //  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //    log.info("registering SSH authentication provider");
 //    auth.authenticationProvider(sshAuthentication());
 //  }
-//
-//  /**
-//   * WebSecurity is used to ask Spring Security to bypass the following resources WebSecurity is
-//   * based on HttpSecurity
-//   */
+
+  /**
+   * WebSecurity is used to ask Spring Security to bypass the following resources WebSecurity is
+   * based on HttpSecurity
+   */
 //  @Override
 //  public void configure(WebSecurity builder) throws Exception {}
-//
-//  @Override
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+            .authorizeHttpRequests(requests -> requests
+                    .anyRequest().permitAll())  // Allow all requests without authentication
+            .formLogin(login -> login
+                    .loginProcessingUrl("/Geoweaver/users/login").permitAll())
+            .logout(withDefaults());
+//            .formLogin(login -> login
+//                    .loginProcessingUrl("/Geoweaver/users/login"))
+//            .logout(withDefaults());
+
+    http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
+    http.csrf(csrf -> csrf.disable());
+
+    return http.build();
+  }
 //  protected void configure(HttpSecurity http) throws Exception {
 //
 //    http.authorizeRequests(requests -> requests
@@ -62,80 +79,15 @@
 //
 //    http.headers(withDefaults()); // this must be turned off to make the JupyterHub work
 //  }
-//
+
+  //need to fix this file
+  //given temp fix
 //  @Bean
-//  CorsConfigurationSource corsConfigurationSource() {
-//    CorsConfiguration configuration = new CorsConfiguration();
-//    configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-//    configuration.setAllowedMethods(Arrays.asList("*"));
-//    configuration.setAllowedHeaders(Arrays.asList("*"));
-//    configuration.setAllowCredentials(true);
-//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//    source.registerCorsConfiguration("/**", configuration);
-//    return source;
+//  public AuthenticationManagerBuilder configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//    log.info("registering SSH authentication provider");
+//    auth.authenticationProvider(sshAuthentication());
+//    return auth;
 //  }
-//
-//  @Bean
-//  public PasswordEncoder passwordEncoder() {
-//    return new BCryptPasswordEncoder();
-//  }
-//}
-
-package com.gw.ssh;
-
-import org.apache.log4j.Logger;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfiguration {
-
-  protected final Logger log = Logger.getLogger(getClass());
-
-  @Bean
-  public SSHAuthenticationProvider sshAuthentication() {
-    return new SSHAuthenticationProvider();
-  }
-
-  @Bean
-  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-    AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-    authenticationManagerBuilder.authenticationProvider(sshAuthentication());
-    return authenticationManagerBuilder.build();
-  }
-
-  @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-            .authorizeHttpRequests(authorize -> authorize
-                    .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-                    .loginProcessingUrl("/Geoweaver/users/login")
-                    .permitAll()
-            )
-            .logout(logout -> logout
-                    .permitAll()
-            )
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .csrf(csrf -> csrf.disable())
-            .headers(headers -> headers.defaultsDisabled()); // This disables the default security headers
-
-    return http.build();
-  }
 
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
@@ -154,3 +106,4 @@ public class SecurityConfiguration {
     return new BCryptPasswordEncoder();
   }
 }
+
--- src/main/java/com/gw/tasks/TaskManager.java
+++ src/main/java/com/gw/tasks/TaskManager.java
@@ -14,6 +14,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -35,7 +36,9 @@ public class TaskManager {
 
   Logger logger = Logger.getLogger(this.getClass());
 
-  @Autowired WorkerManager wm;
+  @Autowired
+  @Lazy
+  WorkerManager wm;
 
   @Autowired BaseTool bt;
 
