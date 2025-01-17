5080dfe29a7dc63e784e75c646b52a1bb35ab98d
Srini Jammula <67372928+srinijammula@users.noreply.github.com>
2025-01-17 15:02:32 -0500

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -258,16 +258,6 @@ describe('Process Testing', () => {
       cy.get('ul#process_folder_python_target').should('contain', 'python_test');
     })
 
-    it('Create Notebook Process', () => {
-      cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
-      cy.get('.introjs-skipbutton').click();
-      cy.get('#newprocess').click();
-      cy.get('form select.form-control.form-control-sm').select('Jupyter Notebook');
-      cy.get('form > :nth-child(1) > :nth-child(4)').clear('t');
-      cy.get('form > :nth-child(1) > :nth-child(4)').type('notebook_test');
-      cy.get('.modal-footer').contains('Add').click();
-      cy.get('ul#process_folder_jupyter_target').should('contain', 'notebook_test');
-    }) 
 });
 
 
@@ -327,16 +317,6 @@ describe('Delete Process', () => {
       cy.get('[style="color:rgb(38, 90, 139);text-align:center;font-family:\'lato\', sans-serif;font-size:80px"]').should('be.visible');
     })
 
-    it('Delete Jupyter Notebook Process', () => {
-      cy.visit('http://localhost:8070/Geoweaver/web/geoweaver');
-      cy.get('.introjs-skipbutton').click();
-      cy.get('#process_folder_jupyter').click();
-      cy.get('ul#process_folder_jupyter_target').contains('notebook_test').click();
-      cy.contains('button', 'Delete').click();
-      cy.get('#del-confirm-btn').click();
-      cy.get('#main-general-content').click();
-      cy.get('[style="color:rgb(38, 90, 139);text-align:center;font-family:\'lato\', sans-serif;font-size:80px"]').should('be.visible');
-    })
 });
 
 describe('Write Password into .secret', () => {
