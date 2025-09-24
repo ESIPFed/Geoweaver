a99b91e0978ce5b2e7aed14a08a9a15087d32583
Ziheng Sun <zsun@gmu.edu>
2025-09-23 21:12:03 -0400

    fix the history table chart color

--- src/main/resources/static/js/gw.js
+++ src/main/resources/static/js/gw.js
@@ -9,7 +9,7 @@ edu = {
         sponsor:
           "ESIPLab incubator project, NASA ACCESS project, NSF Geoinformatics project, NSF Cybertraining project",
 
-        version: "2.0.9",
+        version: "2.1.0",
 
         author: "open source contributors",
 
